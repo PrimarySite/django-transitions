7e4d541d8d061c29ab13f7fc32c2748a3bbdfb85
Christian Ledermann <christian.ledermann@gmail.com>
2018-11-12 12:03:01 +0000

    add circle config

---
+++ testproj/settings_pytest.py
@@ -0,0 +1,13 @@
+from .settings import *
+
+# use an in-memory sqlite db
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    },
+}
